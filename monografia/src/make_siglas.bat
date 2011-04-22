0a06dd10ea4285475f711419d4c00a1eb00c6eca
Christian <6785871+hartungstenio@users.noreply.github.com>
2011-04-22 15:00:26 -0300

    Importa??o inicial

---
+++ monografia/src/make_siglas.bat
@@ -0,0 +1,2 @@
+makeindex -s tabela-simbolos.ist -o TGI.symbols TGI.symbolsx
+pause
---
+++ testes/AprovVal/AprovVal/Controllers/AprovadoresController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Text;
+using System.Configuration;
+
+namespace AprovVal.Controllers
+{
+  public class AprovadoresController : Controller
+  {
+    //
+    // GET: /Aprovadores/
+
+    public ActionResult AprovadoresAteh(string valor)
+    {
+      XmlDocument doc = new XmlDocument();
+      doc.Load(ConfigurationManager.AppSettings["aprovval.xml"]);
+
+      var n = doc.SelectNodes("//VALOR[@VALUE >=" + valor + "]/USERS/USER/@NAME");
+
+      var sb = new StringBuilder();
+
+      for (int i = 0; i < n.Count; i++)
+      {
+        string s = n.Item(i).Value + ';';
+
+        if(!sb.ToString().Contains(s))
+          sb.Append(s);
+      }
+
+      ViewBag.Usuarios = sb.ToString();
+
+      return View();
+    }
+
+  }
+}
