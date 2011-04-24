56f4bf8b37fef5989947a7efbc40876b5192a30a
Christian Hartung <chrishartung@dirtycode.net>
2011-04-24 20:47:16 -0300

    Site para testes
      - Criado site com uma tela de login para testes;
      - Resultado da avalia??o deste site com o WebCruiser;

---
+++ testes/Site/Site/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Xml;
+using System.Web.Security;
+
+namespace Site.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(ConfigurationManager.AppSettings["AuthXML"]);
+
+            try
+            {
+                var x = doc.SelectNodes("//user[@login='" + Request.Form["username"] +
+                                        "' and password/text() = '" + Request.Form["password"] +
+                                        "']/name/text()");
+
+                if (x.Count == 0)
+                {
+                    ViewBag.Message = "Usuário ou senha inválidos";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(Request.Form["username"], false);
+                    Session["Nome"] = x.Item(0).Value;
+                    return RedirectToAction("Details");
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Usuário ou senha inválidos";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize]
+        public ActionResult Details()
+        {
+            ViewBag.Usuario = Session["Nome"];
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
