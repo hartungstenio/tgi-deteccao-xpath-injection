af2f97c91b6675148f6108410011992ddcfdd7b2
Christian <6785871+hartungstenio@users.noreply.github.com>
2011-06-21 23:53:23 -0300

    Apresenta??o + Aplicativo para fazer testes de XPath

---
+++ testes/XPathExec/XPathExec/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XPathExec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("aprovval.xml");
+
+            try
+            {
+                var n = doc.SelectNodes(consulta.Text);
+
+                textBox1.Text = string.Empty;
+
+                for (int i = 0; i < n.Count; i++)
+                    textBox1.Text = textBox1.Text +
+                        n.Item(i).OuterXml +
+                        Environment.NewLine + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Expressão inválida" + Environment.NewLine +
+                                ex.Message);
+
+            }
+        }
+    }
+}
